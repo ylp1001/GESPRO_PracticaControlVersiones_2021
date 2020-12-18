b5dd6292f15a512e7b57e5482558e3313ec7efa4
ylp1001 <75176386+ylp1001@users.noreply.github.com>
2020-12-18 14:25:48 +0100

    Add Crashlytics

--- app/src/prod/java/com/davidmiguel/gobees/Injection.java
+++ app/src/prod/java/com/davidmiguel/gobees/Injection.java
@@ -1,3 +1,21 @@
+package com.davidmiguel.gobees; 
+ 
+import com.davidmiguel.gobees.data.source.repository.GoBeesRepository; 
+import com.davidmiguel.gobees.data.source.local.GoBeesLocalDataSource; 
+import com.davidmiguel.gobees.data.source.network.WeatherDataSource; 
+ 
+/** 
+ * Enables injection of production implementations for GoBeesDataSource at compile time. 
+ */ 
+public class Injection { 
+ 
+    private Injection() { 
+    } 
+ 
+    public static GoBeesRepository provideApiariesRepository() { 
+        return GoBeesRepository.getInstance(GoBeesLocalDataSource.getInstance(), 
+                WeatherDataSource.getInstance()); 
+    } 
 package com.davidmiguel.gobees;
 
 import com.davidmiguel.gobees.data.source.repository.GoBeesRepository;
@@ -16,4 +34,4 @@ public class Injection {
         return GoBeesRepository.getInstance(GoBeesLocalDataSource.getInstance(),
                 WeatherDataSource.getInstance());
     }
-}
+} 
